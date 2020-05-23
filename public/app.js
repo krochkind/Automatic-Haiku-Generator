6cc4f2e7f5d0ee2ca4a99b244cb72cf9cc72d0b8
krochkind <krochkind@gmail.com>
2020-05-23 08:52:13 -0700

    Load background then show haiku

--- public/app.js
+++ public/app.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-    $.get("background", data => {
-        bg = "backgrounds/" + data.background;
+    $.get('background', data => {
+        bg = 'backgrounds/' + data.background;
         $('body').css('background', 'linear-gradient(rgba(255,255,255,.5), rgba(255,255,255,.5)), url(' + bg + ') no-repeat center center fixed');
+        $('#i').attr('src', bg);
     });
 
-    $.get("data", data => {
-        $('#p1').text(data["p1"]);
-        $('#p2').text(data["p2"]);
-        $('#p3').text(data["p3"]);
-    });
+    document.getElementById("i").onload = () => {
+        $.get('data', data => {
+            $('#p1').text(data['p1']);
+            $('#p2').text(data['p2']);
+            $('#p3').text(data['p3']);
+        });
+    };
 });
